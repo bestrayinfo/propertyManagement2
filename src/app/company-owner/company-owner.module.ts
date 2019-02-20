077cf87a1611877dacb1c0c04d62039168ac06e4
deepak <dkdass409@gmail.com>
2019-02-20 12:31:00 +0530

    Update

--- src/app/company-owner/company-owner.module.ts
+++ src/app/company-owner/company-owner.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CompanyOwnerRoutingModule } from './company-owner-routing.module';
+import {RegisterComponent} from './register/register.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
     CompanyOwnerRoutingModule
   ],
-  declarations: []
+  declarations: [
+    RegisterComponent
+  ]
 })
 export class CompanyOwnerModule { }
