bf8a01fddb696fce706965301092f0e8822715df
Dell <del@gmail>
2019-02-20 18:57:47 +0530

    adding registration

--- src/app/company-owner/company-owner.module.ts
+++ src/app/company-owner/company-owner.module.ts
@@ -1,25 +1,29 @@
-import { NgModule } from '@angular/core';
+import * as core from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 import { CompanyOwnerRoutingModule } from './company-owner-routing.module';
 import { RegisterComponent } from './register/register.component';
 import { CompanyOwnerDashboardComponent } from './company-owner-dashboard/company-owner-dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CompanyOwnerService } from './company-owner.service';
 
 
 
 // @ts-ignore
 // @ts-ignore
 // @ts-ignore
-@NgModule({
+@core.NgModule({
   imports: [
     NgbModule, NgbPaginationModule, NgbAlertModule,
     CommonModule,
-    CompanyOwnerRoutingModule
+    CompanyOwnerRoutingModule,ReactiveFormsModule,HttpClientModule
   ],
   declarations: [
     RegisterComponent,
     CompanyOwnerDashboardComponent
-  ]
+  ],
+  providers: [CompanyOwnerService]
 })
 export class CompanyOwnerModule { }
--- src/app/company-owner/register/register.component.ts
+++ src/app/company-owner/register/register.component.ts
@@ -1,16 +1,95 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CompanyOwnerService } from '../company-owner.service';
 
 // @ts-ignore
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
+  ,providers:[CompanyOwnerService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  
+  registerForm: FormGroup;
+  isValidFormSubmitted = false;
+ submitted = false;
 
-  ngOnInit() {
-  }
+ constructor(private router:Router, private formbuilder:FormBuilder,private userService:CompanyOwnerService) { }
+
+ ngOnInit() {
+
+   this.registerForm=this.formbuilder.group({
+     userName:['',Validators.compose([ Validators.required,Validators.minLength(4)])],
+     password:['',Validators.compose([ Validators.required,Validators.minLength(4)])],
+     // repassword:['',Validators.required],
+     firstName:['',Validators.compose([ Validators.required,Validators.minLength(4)])],
+     middleName:['',Validators.compose([ Validators.required,Validators.minLength(4)])],
+     lastName:['',Validators.compose([ Validators.required,Validators.minLength(4)])],
+     phoneHome:['',Validators.compose([ Validators.required])],
+     email:['',Validators.email],
+     phoneOffice:['',Validators.compose([ Validators.required])],
+     
+     phoneMobile:['',Validators.compose([ Validators.required])],
+     
+     address:this.formbuilder.group({
+
+         address1:['',Validators.compose([ Validators.required])],
+         address2:['',Validators.compose([ Validators.required])],
+         city:['',Validators.compose([ Validators.required])],
+         state:['',Validators.compose([ Validators.required])],
+         zip1:['',Validators.compose([ Validators.required])],
+         zip2:['',Validators.compose([ Validators.required])]
+
+      
+
+       }),
+     company:this.formbuilder.group({
+         name:['',Validators.compose([ Validators.required])],
+         ein:['',Validators.compose([ Validators.required])],
+         stateRegNo:['',Validators.compose([ Validators.required])],
+         noOfUnits:['',Validators.compose([ Validators.required])],
+         address:this.formbuilder.group({
+           address1:['',Validators.compose([ Validators.required])],
+           address2:['',Validators.compose([ Validators.required])],
+           city:['',Validators.compose([ Validators.required])],
+           state:['',Validators.compose([ Validators.required])],
+           zip1:['',Validators.compose([ Validators.required])],
+           zip2:['',Validators.compose([ Validators.required])]
+         })
+       })
+     
+   });
+ }
+
+ onSubmit(){
+
+   console.log(this.registerForm.value);
+   this.submitted = true;
+
+
+ 
+   this.createCompanyOwner();
+
+
+ }
+ 
+ createCompanyOwner()
+ {
+
+   this.userService.addCompanyOwner(this.registerForm.value)
+   .subscribe(data=>{
+     alert("user register successfully")
+     console.log(data)
+    //  this.router.navigateByUrl('message')
+   },err=>{
+         console.log(err);
+         alert("problem");
+   }
+   
+   )
+ }
 
 }
