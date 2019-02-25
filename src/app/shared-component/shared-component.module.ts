3252d85d7debe4859d2a4a86c4546fd06f8cee7d
Dell <del@gmail>
2019-02-25 18:15:43 +0530

    alertService add

--- src/app/shared-component/shared-component.module.ts
+++ src/app/shared-component/shared-component.module.ts
@@ -8,14 +8,16 @@ import { httpInterceptorProviders } from './auth/auth-interceptor';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './auth/auth.service';
 import { TokenStorageService } from './auth/token-storage.service';
+import {AlertComponent} from './alert/alert.component';
+import {AlertService} from './service/alert.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedComponentRoutingModule,ReactiveFormsModule,HttpClientModule
+    SharedComponentRoutingModule, ReactiveFormsModule, HttpClientModule
   ],
-  exports:[LoginComponent],
-  // providers:[AuthService,TokenStorageService],
-  declarations: [LoginComponent]
+  exports: [LoginComponent, AlertComponent],
+   providers:[AlertService],
+  declarations: [LoginComponent , AlertComponent]
 })
 export class SharedComponentModule { }
